3447add9ed5679a0411b0b2898a09434d560fdb1
Takahiro Iizuka <agdjgt@gmail.com>
2022-05-04 19:19:28 +0900

    Finished Exercise3 first question

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -15,13 +15,13 @@ class ExercisesController < ApplicationController
     # 【要件】配達先の一番多い住所を返すこと
     #   * joinsを使うこと
     #   * 取得したAddressのインスタンスにorders_countと呼びかけると注文の数を返すこと
-    @address = Address
+    @address = Address.joins(:orders).group(:id).count.max
   end
 
   def exercise4 
     # 【要件】一番お金を使っている顧客を返すこと
     #   * joinsを使うこと
     #   * 取得したCustomerのインスタンスにfoods_price_sumと呼びかけると合計金額を返すこと
-    @customer = Customer
+    @customer = Customer.joins(:)
   end
 end
