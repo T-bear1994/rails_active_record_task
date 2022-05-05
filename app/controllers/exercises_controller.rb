65f86c46911af492f097b1fe84baaacf2cd93713
Takahiro Iizuka <agdjgt@gmail.com>
2022-05-05 09:52:01 +0900

    Finished Exercise4

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -22,6 +22,6 @@ class ExercisesController < ApplicationController
     # 【要件】一番お金を使っている顧客を返すこと
     #   * joinsを使うこと
     #   * 取得したCustomerのインスタンスにfoods_price_sumと呼びかけると合計金額を返すこと
-    @customer = Customer.joins(orders: [order_foods: :foods])
+    @customer = Customer.joins(orders: [order_foods: :foods]).select("customers.*, SUM(foods.price) as foods_price_sum").order("foods_price_sum DESC").first
   end
 end
